82ec31ef3cfda41452907090f855e944bb08663c
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-10-10 17:53:03 -0400

    reformatting code

--- Part 0 - Warm-up/cat_dog.py
+++ Part 0 - Warm-up/cat_dog.py
@@ -9,10 +9,8 @@ def cat_dog(str):
 
     for i in range(len(str)-2):
 
-        if str[i] + str[i+1] + str[i+2] == 'cat':
-            cat += 1
-        elif str[i] + str[i+1] + str[i+2] == 'dog':
-            dog += 1
+        if str[i] + str[i+1] + str[i+2] == 'cat': cat += 1
+        elif str[i] + str[i+1] + str[i+2] == 'dog': dog += 1
 
     if cat == dog:
         return True
