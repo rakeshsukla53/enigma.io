9481fab91182fb06b83677e341135e3015c7223b
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-10-10 02:32:23 -0400

    Assignment 0

---
+++ Part 1 - CSV Test/code_swap/code_swap.py
@@ -0,0 +1,14 @@
+__author__ = 'rakesh'
+
+import csv
+
+with open('solution_state_abbreviations.csv', 'wb') as f: # output csv file
+    writer = csv.writer(f)
+    with open('state_abbreviations.csv','r') as csvfile: # input csv file
+        reader = csv.reader(csvfile, delimiter=',')
+        count = 0
+        for row in reader:
+            row[0] = row[1]  #replacing each state abbreviation with its associate state name
+            writer.writerow(row) #write to the solution file
+f.close()
+
---
+++ Part 1 - CSV Test/string_cleaning/string_cleaning.py
@@ -0,0 +1,22 @@
+__author__ = 'rakesh'
+
+import csv
+
+with open('solution_test.csv', 'wb') as f: # output csv file
+    writer = csv.writer(f)
+    with open('test.csv','r') as csvfile: # input csv file
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            row[8] = " ".join(row[8].split()).strip()  #removing extra spaces from the string
+            writer.writerow(row) #writing to a new file
+f.close()
+
+
+
+
+
+
+
+
+
+
