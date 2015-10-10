9fcb7d223186ca9900d72c40efc43baf02165887
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-10-10 03:52:53 -0400

    Warm up problems

---
+++ Part 0 - Warm-up/count_code.py
@@ -0,0 +1,15 @@
+__author__ = 'rakesh'
+
+def count_code(str):
+
+    count = 0
+    if len(str) < 4:
+        return 0
+
+    for i in range(len(str)-3):
+        if str[i] == 'c' and str[i+1] == 'o' and str[i+3] == 'e':
+            count += 1
+
+    return count
+
+
